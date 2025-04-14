9cb3eb730a13faff527c70ffd36ddb539a517873
jmielc2 <Jacob.mielczarek@gmail.com>
2025-04-13 19:36:20 -0500

    Generating spectrum
    Writing into the file is behaving weirdly though

--- Assets/Scripts/WaveHeightGenerator.cs
+++ Assets/Scripts/WaveHeightGenerator.cs
@@ -2,26 +2,51 @@ using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class WaveHeightGenerator : MonoBehaviour {
-    GameObject displayQuad;
-    Material displayMaterial;
+    public ComputeShader compute;
+    public RenderTexture spectrum_texture;
+    [Range(64, 1024)]
+    public int N;
+
+    private GameObject display_quad;
+    private bool compute_configured = false;
+    private const int init_spectrum_kernel = 0;
 
     void Start() {
         CreateDisplayQuad();
+        CreateSpectrumTexture();
     }
 
     void OnDestroy() {
-        Destroy(displayQuad);
-        Destroy(displayMaterial);
+        Destroy(display_quad);
+        spectrum_texture.Release();
+    }
+
+    private void Update() {
+        if (!compute_configured) {
+            compute.SetInt("u_N", N);
+            compute.SetFloat("u_scale", Mathf.PI / 128f);
+            compute.SetTexture(init_spectrum_kernel, "initial_spectrum", spectrum_texture);
+            compute.Dispatch(init_spectrum_kernel, N / 8, N / 8, 1);
+            compute_configured = true;
+        }
+    }
+
+    private void CreateSpectrumTexture() {
+        spectrum_texture = new RenderTexture(N, N, 0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear) {
+            filterMode = FilterMode.Bilinear,
+            wrapMode = TextureWrapMode.Repeat,
+            enableRandomWrite = true,
+            useMipMap = true,
+            autoGenerateMips = false,
+            anisoLevel = 16,
+        };
+        spectrum_texture.Create();
     }
 
     private void CreateDisplayQuad() {
         // Create a quad
-        displayQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        displayQuad.transform.position = transform.position + transform.forward * 2f; // Place in front of camera
-        displayQuad.transform.localScale = new Vector3(1f, 1f, 1f) * 2f;
-        
-        // Create a material
-        displayMaterial = new Material(Shader.Find("Custom/Height Map"));
-        displayQuad.GetComponent<MeshRenderer>().material = displayMaterial;
+        display_quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        display_quad.transform.position = transform.position + transform.forward * 2f; // Place in front of camera
+        display_quad.transform.localScale = new Vector3(1f, 1f, 1f) * 2f;
     }
 }
