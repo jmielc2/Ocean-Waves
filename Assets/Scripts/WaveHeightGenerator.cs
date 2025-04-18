da4bad8c5d53fe0c585f30693ad75e426d410385
jmielc2 <Jacob.mielczarek@gmail.com>
2025-04-17 21:40:47 -0500

    Preparing for IFFT step!

--- Assets/Scripts/WaveHeightGenerator.cs
+++ Assets/Scripts/WaveHeightGenerator.cs
@@ -4,51 +4,57 @@ using UnityEngine;
 public class WaveHeightGenerator : MonoBehaviour {
     public ComputeShader compute;
     public RenderTexture spectrum_texture;
+    public RenderTexture height_texture;
     [Range(64, 1024)]
     public int N;
 
-    private GameObject display_quad;
     private bool compute_configured = false;
     private const int init_spectrum_kernel = 0;
+    private const int generate_height_spectrum_kernel = 1;
 
     void Start() {
-        CreateDisplayQuad();
         CreateSpectrumTexture();
+        CreateHeightTexture();
     }
 
     void OnDestroy() {
-        Destroy(display_quad);
         spectrum_texture.Release();
+        height_texture.Release();
     }
 
     private void Update() {
         if (!compute_configured) {
-            compute.SetTexture(init_spectrum_kernel, "initial_spectrum", spectrum_texture);
+            compute.SetTexture(init_spectrum_kernel, "spectrum_texture", spectrum_texture);
+            compute.SetTexture(generate_height_spectrum_kernel, "spectrum_texture", spectrum_texture);
+            compute.SetTexture(generate_height_spectrum_kernel, "height_texture", height_texture);
             compute.SetInt("u_N", N);
             compute.SetFloat("u_L", 256f);
             compute.SetVector("u_wind_direction", new Vector4(1, 1, 0, 0).normalized);
             compute.SetFloat("u_wind_speed", 5f);
             compute.Dispatch(init_spectrum_kernel, N / 8, N / 8, 1);
+            compute.Dispatch(generate_height_spectrum_kernel, N / 8, N / 8, 1);
             compute_configured = true;
         }
     }
 
-    private void CreateSpectrumTexture() {
-        spectrum_texture = new RenderTexture(N, N, 0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear) {
+    private RenderTexture CreateRenderTexture(int width, int height, int depth, RenderTextureFormat format) {
+        var texture = new RenderTexture(width, height, depth, format, RenderTextureReadWrite.Linear) {
             filterMode = FilterMode.Bilinear,
             wrapMode = TextureWrapMode.Repeat,
             enableRandomWrite = true,
             useMipMap = true,
             autoGenerateMips = false,
-            anisoLevel = 16,
+            anisoLevel = 16
         };
-        spectrum_texture.Create();
+        texture.Create();
+        return texture;
     }
 
-    private void CreateDisplayQuad() {
-        // Create a quad
-        display_quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        display_quad.transform.position = transform.position + transform.forward * 2f; // Place in front of camera
-        display_quad.transform.localScale = new Vector3(1f, 1f, 1f) * 2f;
+    private void CreateSpectrumTexture() {
+        spectrum_texture = CreateRenderTexture(N, N, 0, RenderTextureFormat.RGFloat);
+    }
+
+    private void CreateHeightTexture() {
+        height_texture = CreateRenderTexture(N, N, 0, RenderTextureFormat.RFloat);
     }
 }
