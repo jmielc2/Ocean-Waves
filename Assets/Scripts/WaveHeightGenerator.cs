375eaa9535f74416facde25f5c356ba873555cc4
jmielc2 <Jacob.mielczarek@gmail.com>
2025-05-03 21:52:43 -0500

    Finished first attempt
    Doesn't work, but overall process is in place

--- Assets/Scripts/WaveHeightGenerator.cs
+++ Assets/Scripts/WaveHeightGenerator.cs
@@ -5,7 +5,6 @@ public class WaveHeightGenerator : MonoBehaviour {
     public ComputeShader compute;
     public RenderTexture spectrum_texture;
     public RenderTexture height_texture;
-    public RenderTexture butterfly_texture;
     public RenderTexture fourier_texture;
 
     [Range(64, 1024)]
@@ -14,52 +13,41 @@ public class WaveHeightGenerator : MonoBehaviour {
     private int N_times_N_log2;
     private bool compute_configured = false;
     private const int init_spectrum_kernel = 0;
-    private const int generate_butterfly_texture_kernel = 1;
-    private const int cycle_through_time_kernel = 2;
-    private const int horizontal_ifft_kernel = 3;
-    private const int vertical_ifft_kernel = 4;
+    private const int cycle_through_time_kernel = 1;
+    private const int horizontal_ifft_kernel = 2;
+    private const int vertical_ifft_kernel = 3;
 
     void Start() {
         N_times_N_log2 = (int)Mathf.Log(N * N, 2.0f);
         CreateSpectrumTexture();
-        CreateButterflyTexture();
         CreateFourierTexture();
         CreateHeightTexture();
     }
 
     void OnDestroy() {
         spectrum_texture.Release();
-        butterfly_texture.Release();
         fourier_texture.Release();
         height_texture.Release();
     }
 
     private void Update() {
         if (!compute_configured) {
-            float time = 100f;
+            float time = Time.time;
             compute.SetTexture(init_spectrum_kernel, "spectrum_texture", spectrum_texture);
-            compute.SetTexture(generate_butterfly_texture_kernel, "butterfly_texture", butterfly_texture);
             compute.SetTexture(cycle_through_time_kernel, "spectrum_texture", spectrum_texture);
             compute.SetTexture(cycle_through_time_kernel, "fourier_texture", fourier_texture);
-            compute.SetTexture(horizontal_ifft_kernel, "butterfly_texture", butterfly_texture);
-            compute.SetTexture(vertical_ifft_kernel, "butterfly_texture", butterfly_texture);
+            compute.SetTexture(horizontal_ifft_kernel, "fourier_texture", fourier_texture);
+            compute.SetTexture(vertical_ifft_kernel, "fourier_texture", fourier_texture);
+            compute.SetTexture(vertical_ifft_kernel, "height_texture", height_texture);
             compute.SetInt("u_N", N);
             compute.SetFloat("u_L", 256f);
             compute.SetFloat("u_time", time);
             compute.SetVector("u_wind_direction", new Vector4(1, 1, 0, 0).normalized);
             compute.SetFloat("u_wind_speed", 5f);
             compute.Dispatch(init_spectrum_kernel, N / 8, N / 8, 1);
-            compute.Dispatch(generate_butterfly_texture_kernel, N_times_N_log2, N * N / 64, 1);
             compute.Dispatch(cycle_through_time_kernel, N / 8, N / 8, 1);
-            // Do Horizontal and Vertical IFFTs
-            int pingpong_direction = 0;
-            for (int i = 0; i < N_times_N_log2; i++) {
-                compute.SetInt("pingpong_direction", pingpong_direction);
-                compute.SetInt("pingpong_iteration", i);
-                compute.SetTexture(horizontal_ifft_kernel, "pingpong0", fourier_texture);
-                compute.SetTexture(horizontal_ifft_kernel, "pingpong1", fourier_texture);
-            }
-            
+            compute.Dispatch(horizontal_ifft_kernel, 1, N, 1);
+            compute.Dispatch(vertical_ifft_kernel, N, 1, 1);
             compute_configured = true;
         }
     }
@@ -79,10 +67,6 @@ public class WaveHeightGenerator : MonoBehaviour {
 
     private void CreateSpectrumTexture() {
         spectrum_texture = CreateRenderTexture(N, N, 0, RenderTextureFormat.ARGBFloat);
-    }
-
-    private void CreateButterflyTexture() {
-        butterfly_texture = CreateRenderTexture(N_times_N_log2, N * N, 0, RenderTextureFormat.ARGBFloat);
     }
 
     private void CreateFourierTexture() {
