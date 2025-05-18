aab156a23f98d911f15f07c4d7ad7b21c66abc8b
Jacob Mielczarek <Jacob.mielczarek@gmail.com>
2025-05-18 17:21:20 -0500

    Added packing of conjugate to initial spectrum instead of generating it on the fly

--- Assets/Scripts/WaveHeightGenerator.cs
+++ Assets/Scripts/WaveHeightGenerator.cs
@@ -2,22 +2,27 @@ using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class WaveHeightGenerator : MonoBehaviour {
+    public enum Resolution {
+        TWO_FIFTY_SIX = 256, FIVE_TWELVE = 512, TEN_TWENTY_FOUR = 1024
+    }
+
     public ComputeShader compute;
     public RenderTexture spectrum_texture;
     public RenderTexture height_texture;
     public RenderTexture fourier_texture;
+    public Resolution resolution = Resolution.TEN_TWENTY_FOUR;
 
-    [Range(64, 1024)]
-    public int N;
-
+    private int N;
     private int N_times_N_log2;
     private bool compute_configured = false;
     private const int init_spectrum_kernel = 0;
-    private const int cycle_through_time_kernel = 1;
-    private const int horizontal_ifft_kernel = 2;
-    private const int vertical_ifft_kernel = 3;
+    private const int pack_minus_k_conj_kernel = 1;
+    private const int cycle_through_time_kernel = 2;
+    private const int horizontal_ifft_kernel = 3;
+    private const int vertical_ifft_kernel = 4;
 
     void Start() {
+        N = (int)resolution;
         N_times_N_log2 = (int)Mathf.Log(N * N, 2.0f);
         CreateSpectrumTexture();
         CreateFourierTexture();
@@ -34,6 +39,7 @@ public class WaveHeightGenerator : MonoBehaviour {
         if (!compute_configured) {
             float time = Time.time + 10.0f;
             compute.SetTexture(init_spectrum_kernel, "spectrum_texture", spectrum_texture);
+            compute.SetTexture(pack_minus_k_conj_kernel, "spectrum_texture", spectrum_texture);
             compute.SetTexture(cycle_through_time_kernel, "spectrum_texture", spectrum_texture);
             compute.SetTexture(cycle_through_time_kernel, "fourier_texture", fourier_texture);
             compute.SetTexture(horizontal_ifft_kernel, "fourier_texture", fourier_texture);
@@ -45,9 +51,10 @@ public class WaveHeightGenerator : MonoBehaviour {
             compute.SetVector("u_wind_direction", new Vector4(1, 1, 0, 0).normalized);
             compute.SetFloat("u_wind_speed", 15f);
             compute.Dispatch(init_spectrum_kernel, N / 8, N / 8, 1);
+            compute.Dispatch(pack_minus_k_conj_kernel, N / 8, N / 8, 1);
             compute.Dispatch(cycle_through_time_kernel, N / 8, N / 8, 1);
             compute.Dispatch(horizontal_ifft_kernel, 1, N, 1);
-            // compute.Dispatch(vertical_ifft_kernel, N, 1, 1);
+            compute.Dispatch(vertical_ifft_kernel, N, 1, 1);
             compute_configured = true;
         }
     }
